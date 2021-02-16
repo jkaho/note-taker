b4183b5ac7bd4a2496b6cc381aff4ba6982d2731
jkaho <72959522+jkaho@users.noreply.github.com>
2021-02-16 14:16:57 +1100

    Modify 'server.js' to add router and listener

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// Router
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// Listener 
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
